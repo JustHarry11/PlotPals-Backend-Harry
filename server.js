b5df042044a0943053959c01a63f8236218a0623
CLiu <callumliu09@hotmail.co.uk>
2025-05-13 14:16:29 +0100

    PlotPals initial backend start

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
+import 'dotenv/config'
+import cors from 'cors'
+
+
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+
+app.use('/{*any}', (req, res) => {
+    return res.status(404).json({ message: 'Route not found' })
+})
+
+
+const startServers = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('Database connected');
+
+        app.listen(port, () => console.log('Server is now running'))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServers()
