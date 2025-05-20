4d05c5de0cfa961cd2a2bcddf95e76771d00896a
CLiu <callumliu09@hotmail.co.uk>
2025-05-20 22:22:25 +0100

    Added variant login, username or email

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,7 @@ export default function errorHandler(err, res) {
     console.log(err)
     let { name, status, field, message, code, kind } = err
 
-    if( name === "ValidationError" ){
+    if (name === "ValidationError" ){
         const fields = Object.keys(err.errors)
         const responseBody = {}
         fields.forEach(field => {
@@ -11,17 +11,21 @@ export default function errorHandler(err, res) {
         return res.status(422).json(responseBody)
     }
 
-    if ( name === "MongoServerError" && code === 11000 ) {
+    if (name === "MongoServerError" && code === 11000 ) {
         const field = Object.keys(err.keyValue)[0]
         return res.status(422).json({ [field]: `${field} is already taken` })
     }
 
-    if ( name === "JsonWebTokenError" ) {
+    if (name === "JsonWebTokenError" ) {
         return res.status(401).json({ message: 'Unauthorized'})
     }
 
     if (name === 'CastError' && kind === 'ObjectId') {
         return res.status(422).json({ message: 'Invalid ObjectId'})
+    }
+
+    if (name === 'TokenExpiredError') {
+        return res.status(401).json({ message: 'Session expired. Please log in again.'})
     }
 
     if (!status) {
